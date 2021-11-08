ff7d512b8e422bada88c58ca40978556bf4aaaf6
adrian.turculet@yahoo.com <adrian.turculet@yahoo.com>
2021-11-08 15:51:47 +0000

    update service_class_current

--- models/service_class_current.sql
+++ models/service_class_current.sql
@@ -1,37 +1,22 @@
 /* {{ config(materialized='table') }} */
 
 
-with product_operations as (
 
-    select *
-
-    from product_operations.item_ledger_entries
-
-),
-
-stock as (
-
-    select *
-
-    from product_operations.stock
-
-),
-
-ledger_stock as (
+with ledger_stock as (
 
     select
         posting_date,
         SUM(quantity) as total_quantity,
         service_class
 
-    from product_operations 
+    from product_operations.item_ledger_entries
 
-    left join stock using (item_no_, location_code)
+    left join product_operations.stock using (item_no_, location_code)
 
     group by 1,3
     order by 1,3 asc
 )
 
 
-select * from ledger_stock;
+select * from ledger_stock
 
